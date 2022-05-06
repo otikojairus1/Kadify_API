4ccf0f575ab8baa3793e8557f7f808930fc69f32
realOtiko <you@example.com>
2022-05-07 02:12:54 +0300

    added auth endpoints

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Validator;
 use App\Models\EmailOtpVerifier;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
 
 class UserController extends Controller
 {
@@ -38,9 +43,25 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function register(Request $request)
     {
-        //
+        $rules = [
+            'fullName' => 'required',
+            'email' => 'unique:users|required',
+            'password'    => 'required',
+        ];
+    
+        $input     = $request->only('fullName', 'email','password');
+        $validator = Validator::make($input, $rules);
+    
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'error' => $validator->messages()]);
+        }
+    
+        $user     = User::create(['fullName' => $request->fullName, 'email' => $request->email, 'password' => Hash::make($request->password)]);
+        //$token = $request->name->createToken('accessToken');
+        return response()->json(['responseCode' => 200, 'message' => 'user has registered successfully.']);
+     
     }
 
     /**
@@ -49,10 +70,30 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
+
+    public function login(Request $request){ 
+        date_default_timezone_set('Africa/Nairobi');
+        $rules = [
+          
+            'email' => 'required',
+            'password'    => 'required',
+        ];
+    
+        $input     = $request->only('email','password');
+        $validator = Validator::make($input, $rules);
+    
+        if ($validator->fails()) {
+            return response()->json(['responseCode' => 401, 'error' => $validator->messages()]);
+        }
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            return response()->json(['responseCode'=>200,'userDetails'=>$user, 'loginTime'=>Carbon::now()->toDateTimeString()]); 
+        } 
+        else{ 
+            return response()->json(['responseCode'=>401,'error'=>'wrong login credentials']); 
+        } 
     }
+
 
     /**
      * Show the form for editing the specified resource.
@@ -62,7 +103,7 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        
     }
 
     /**
@@ -83,9 +124,29 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function updatephone(Request $request)
     {
-        //
+              //start of route protection
+              $rules = [
+          
+                'email' => 'required',
+                'phone' => 'required',
+              
+             
+            ];
+    
+            $input     = $request->only('email', 'phone');
+            $validator = Validator::make($input, $rules);
+    
+            if ($validator->fails()) {
+                return response()->json(['success' => false, 'error' => $validator->messages()]);
+            }
+
+            // perform phone update
+            $user = User::where('email', $request->email)->first();
+            $user->phone = $request->phone;
+            $user->save();
+        return response()->json(["response"=>"success"]);
     }
 
     public function send_email_otp(Request $request)
@@ -93,18 +154,22 @@ class UserController extends Controller
         //start of route protection
         $rules = [
           
-            'email' => 'unique:users|required',
-            'name' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required',
+            'fullName' => 'required'
          
         ];
 
-        $input     = $request->only('email', 'name');
+        $input     = $request->only('email', 'fullName', 'password');
         $validator = Validator::make($input, $rules);
 
         if ($validator->fails()) {
             return response()->json(['success' => false, 'error' => $validator->messages()]);
         }
 
+        User::create(['fullName' => $request->fullName, 'email' => $request->email, 'password' => Hash::make($request->password)]);
+        //$token = $request->name->createToken('accessToken');
+       // return response()->json(['responseCode' => 200, 'message' => 'user has registered successfully.']);
 
         // end of route protection
         $otpCode = mt_rand(1000,9999);
@@ -125,7 +190,7 @@ class UserController extends Controller
                         ],
                         'Subject' => "KADIFY VERIFICATION OTP",
                         'TextPart' => "Greetings from Kadify!",
-                        'HTMLPart' => "<h3>Greetings from Kadify,<br> welcome to our Finance world Use<h3> <h1>".$otpCode."</h1> <h3>as your verification code for your Kadify account</h3>"
+                        'HTMLPart' => "<h3>Greetings from Kadify,<br> welcome to our Finance world<h3> <h1>".$otpCode."</h1> <h3>is your verification code for your Kadify account</h3>"
                     ]
                 ]
             ];
@@ -133,11 +198,59 @@ class UserController extends Controller
             $response->success();
             // var_dump($response->getData());
             // have the code backup on the server
-           EmailOtpVerifier::create([
+        //    EmailOtpVerifier::create([
+        //         'email'=> $request->email,
+        //         'code'=>$otpCode
+        //     ]);
+
+            $userToUpdate = EmailOtpVerifier::where('email', $request->email)->first();
+
+            if($userToUpdate){
+                $userToUpdate->code = $otpCode;
+                $userToUpdate->save();
+            }else{
+                EmailOtpVerifier::create([
                 'email'=> $request->email,
                 'code'=>$otpCode
             ]);
+            }
+
+          
 
         return response()->json(["response"=>"success", "otpsent"=>$otpCode, "data"=>$response->getData()]);
+    }
+
+    public function verify_email_otp(Request $request)
+    {
+        date_default_timezone_set('Africa/Nairobi');
+              //start of route protection
+              $rules = [
+          
+                'email' => 'required',
+                'otp' => 'required',
+             
+            ];
+    
+            $input     = $request->only('email', 'otp');
+            $validator = Validator::make($input, $rules);
+    
+            if ($validator->fails()) {
+                return response()->json(['success' => false, 'error' => $validator->messages()]);
+            }
+
+            // end of route protection
+
+            $checkOTP = EmailOtpVerifier::where(['email'=> $request->email, 'code'=>$request->otp])->first();
+            if($checkOTP){
+                // $updateUserVerification = User::where('email', $request->email)->first();
+                // $updateUserVerification->email_verified_at = Carbon::now()->toDateTimeString();
+                // $updateUserVerification->save();
+                  
+                return response()->json(["responseCode"=>200, "VerificationStatus"=> true]);
+            }else{
+                return response()->json(["responseCode"=>401, "VerificationStatus"=> false]);
+            }
+    
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,19 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// OTP EMAIL VERIFICATION ENDPOINTS
 Route::post('/send/email/otp', [\App\Http\Controllers\UserController::class, "send_email_otp"]);
+Route::post('/verify/email/otp', [\App\Http\Controllers\UserController::class, "verify_email_otp"]);
+
+//USER AUTHENTICATION ENDPOINTS
+Route::post('/register', [\App\Http\Controllers\UserController::class, "register"]);
+Route::post('/login', [\App\Http\Controllers\UserController::class, "login"]);
+Route::post('/update/verified/phone', [\App\Http\Controllers\UserController::class, "updatephone"]);
